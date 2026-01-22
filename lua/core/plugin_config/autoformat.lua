efb7c9d5d4bc8132fc0b69d0a6e42ca30c0e24e5
Marco Chan Witzel <marco.chan-witzel@epita.fr>
2026-01-22 23:29:54 +0100

    feat: yaml

--- lua/core/plugin_config/autoformat.lua
+++ lua/core/plugin_config/autoformat.lua
@@ -41,6 +41,7 @@ require('conform').setup {
     javascript = { 'prettierd', 'prettier', stop_after_first = true },
     c = { 'clang-format' },
     sql = { 'sqlfluff' },
+    yml = { 'yamllint' },
   },
 }
 
