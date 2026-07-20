92ae3b2c146ada81616b086c357543fdef07c8a6
Chan Witzel Marco <marco.chan-witzel@epita.fr>
2026-07-20 19:49:03 +0200

    fix: telescope and init format

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -23,8 +23,7 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter'
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    requires = { { 'nvim-lua/plenary.nvim' } },
+    requires = { { 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
   }
   use {
     'williamboman/mason.nvim',
